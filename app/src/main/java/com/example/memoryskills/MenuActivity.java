1ea6868772bda06a04bc6336d2c96f7034d6c5be
Poonam <poonambellu95@gmail.com>
2019-09-11 17:57:28 +0530

    Sept 11 2019

--- app/src/main/java/com/example/memoryskills/MenuActivity.java
+++ app/src/main/java/com/example/memoryskills/MenuActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_scrolling );
+        setContentView( R.layout.activity_menu );
     }
 }
